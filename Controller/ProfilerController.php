63eb13567d94e4a36cef41aaa0621f54e177603d
eXtreme <jacek.jedrzejewski@gmail.com>
2012-03-29 19:41:37 +0200

    fix: keep sessions flashes

--- Controller/ProfilerController.php
+++ Controller/ProfilerController.php
@@ -5,6 +5,8 @@ namespace Profiler\LiveBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Profiler\LiveBundle\Profiler\LiveProfile;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\HttpFoundation\Session\Flash\AutoExpireFlashBag;
 
 class ProfilerController extends Controller
 {
@@ -18,12 +20,27 @@ class ProfilerController extends Controller
      */
     public function updateAction($token = '')
     {
+        $request = $this->container->get('request');
+        
+        if (version_compare(Kernel::VERSION, '2.1.0-dev', '<')) {
+            if (null !== $session = $request->getSession()) {
+                // keep current flashes for one more request
+                $session->setFlashes($session->getFlashes());
+            }
+        } else {
+            $session = $request->getSession();
+            
+            if (null !== $session && $session->getFlashBag() instanceof AutoExpireFlashBag) {
+                // keep current flashes for one more request if using AutoExpireFlashBag
+                $session->getFlashBag()->setAll($session->getFlashBag()->peekAll());
+            }
+        }
+
         // We use the profile token for the client to indicate the last token they
         // retrieved. The token is simple a uniqid(). If a token is not present
         // we just create one
-        if (strlen($token) === 0)
-        {
-            $token   = uniqid();
+        if (strlen($token) === 0) {
+            $token = uniqid();
         }
 
         // Get the latest tokens
